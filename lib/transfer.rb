0e18ba6b8706fcff44bfc5bd44c76f3e2dd27bb2
Adam Duff <j.adam.duff@gmail.com>
2018-08-17 19:26:03 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,10 +18,14 @@ class Transfer
 
   def execute_transaction
     if self.valid? && self.status == "pending"
-
+      self.sender.balance = self.sender.balance - self.amount
+      self.receiver.balance = self.receiver.balance + self.amount
+      self.status = "complete"
     elsif !self.valid?
+      self.status = "rejected"
       "Transaction rejected. Please check your account balance."
-      
+    end
+
   end
 
 end
