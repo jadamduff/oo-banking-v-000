95bcfa39330700624381c1ee455d8d9a1f43b635
Adam Duff <j.adam.duff@gmail.com>
2018-08-17 19:39:57 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -27,4 +27,12 @@ class Transfer
     end
   end
 
+  def reverse_transfer
+    if self.status == "complete"
+      self.sender.balance = self.sender.balance + self.amount
+      self.receiver.balance = self.receiver.balance - self.amount
+      self.status = "pending"
+    end
+  end
+
 end
